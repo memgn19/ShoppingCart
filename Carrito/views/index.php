bfcd930ea2e49a6e3e99c634cd96972a804aa581
Elena <113547059+memgn19@users.noreply.github.com>
2024-08-30 20:03:16 +0200

    Some styles changed

--- Carrito/views/index.php
+++ Carrito/views/index.php
@@ -16,7 +16,7 @@ session_start();
     <symbol id="bootstrap" viewBox="0 0 118 94">
       <path fill-rule="evenodd" clip-rule="evenodd" d="M24.509 0c-6.733 0-11.715 5.893-11.492 12.284.214 6.14-.064 14.092-2.066 20.577C8.943 39.365 5.547 43.485 0 44.014v5.972c5.547.529 8.943 4.649 10.951 11.153 2.002 6.485 2.28 14.437 2.066 20.577C12.794 88.106 17.776 94 24.51 94H93.5c6.733 0 11.714-5.893 11.491-12.284-.214-6.14.064-14.092 2.066-20.577 2.009-6.504 5.396-10.624 10.943-11.153v-5.972c-5.547-.529-8.934-4.649-10.943-11.153-2.002-6.484-2.28-14.437-2.066-20.577C105.214 5.894 100.233 0 93.5 0H24.508zM80 57.863C80 66.663 73.436 72 62.543 72H44a2 2 0 01-2-2V24a2 2 0 012-2h18.437c9.083 0 15.044 4.92 15.044 12.474 0 5.302-4.01 10.049-9.119 10.88v.277C75.317 46.394 80 51.21 80 57.863zM60.521 28.34H49.948v14.934h8.905c6.884 0 10.68-2.772 10.68-7.727 0-4.643-3.264-7.207-9.012-7.207zM49.948 49.2v16.458H60.91c7.167 0 10.964-2.876 10.964-8.281 0-5.406-3.903-8.178-11.425-8.178H49.948z"></path>
     </symbol>
-    <symbol id="home" viewBox="0 0 16 16" style="color: white;">
+    <symbol id="home" viewBox="0 0 16 16" style="color: black;">
       <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5z"></path>
     </symbol>
     <symbol id="people-circle" viewBox="0 0 16 16">
@@ -33,24 +33,26 @@ session_start();
       <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z" />
       <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
     </svg>
-    <title>Shop</title>
+    <title>Shopping Cart</title>
 </head>
 <!-- Final head  -->
-<body>
+<body class="bg-body">
   <header>
     <!-- Inicio de navbar-->
-    <div class="py-2 text-bg-dark border-bottom border-dark">
-      <div class="container">
-        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-          <a href="index.php" class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
-            <img src="imgs/logo.png" alt="logo" height="100">
-          </a>
-          <div class="d-flex justify-content-start">
-          <h1 class="me-5">ONLINE SHOP</h1>
-          </div>
+    <div class="py-2">
+  <div class="container">
+    <div class="d-flex flex-wrap align-items-center justify-content-between">
+      <!-- Contenedor para logo y título alineados a la izquierda -->
+      <div class="d-flex align-items-center me-auto">
+        <a href="index.php" class="d-flex align-items-center my-2 my-lg-0 text-white text-decoration-none">
+          <img src="imgs/logo.png" alt="logo" width="100">
+        </a>
+        <h3 class="ms-3 mt-1 title text-black">Shopping Cart</h3>
+      </div>
+        
           <ul id="header" class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
             <li>
-              <a href="index.php" class="nav-link text-white pt-4">
+              <a href="index.php" class="nav-link text-black pt-4">
                 <svg class="bi d-block mx-auto mb-2" width="24" height="24">
                   <use xlink:href="#grid"></use>
                 </svg>
@@ -81,7 +83,7 @@ session_start();
             </li>
             <li>
               <form action="" method="post">
-                <button class="btn btn-outline-dark" type="submit" name="action" value="sessionDestroy">
+                <button class="btn" type="submit" name="action" value="sessionDestroy">
                   <a class="nav-link text-secondary mb-1">
                     <svg id="out_icon" class="bi d-block mx-auto m-2" width="24" height="24">
                       <use class="pb-1 mb-1" xlink:href="#out"></use>
@@ -97,7 +99,7 @@ session_start();
     </div>
     <!-- Final nav bar  -->
     <!-- Inicio buttons  -->
-    <div class="px-3 py-2 border-bottom mb-3 bg-dark">
+    <div class="px-3 py-2 mb-3">
       <div class="container d-flex flex-wrap justify-content-center position-relative">
         <!-- Vista parcial de form login  -->
         <div class="position-fixed mx-auto" id="include">
@@ -113,7 +115,7 @@ session_start();
         </div>
         <!-- Inicio Form Search  -->
         <form class="col-12 col-lg-auto mb-2 mb-lg-0 me-lg-auto" role="search">
-          <input type="search" name="search" class="form-control ms-3" placeholder="Search..." aria-label="Search" value="<?php if (isset($_GET['search'])) {
+          <input id="search" type="search" name="search" class="form-control ms-3" placeholder="Search..." aria-label="Search" value="<?php if (isset($_GET['search'])) {
                                                                                                                             echo $_GET['search'];
                                                                                                                           } ?>">
         </form>
@@ -122,8 +124,8 @@ session_start();
           <!-- Botones desplegable  -->
           <!-- Inicio boton añadir producto provisional obviusly   -->
           <button id="add" onclick="desplegarProduct()" class="btn btn-light ms-2 rounded-2">Add new product</button>
-          <button type="button" id="button2" onclick="desplegarRegistrar()" class="btn btn-light">Sign-up</button>
-          <button type="button" id="button1" onclick="desplegar()" class="btn btn-outline-light  me-2">Login</button>
+          <button type="button" id="button2" onclick="desplegarRegistrar()" class="btn btn-styles">Sign-up</button>
+          <button type="button" id="button1" onclick="desplegar()" class="btn btn-styles me-2">Login</button>
           <!-- Fin de botones desplegables  -->
         </div>
       </div>
@@ -139,7 +141,7 @@ session_start();
     }
     
     ?>
-    <div id="products" class="album py-5 bg-body-tertiary">
+    <div id="products" class="album py-5 ">
       <div class="container">
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
           <?php
@@ -219,10 +221,10 @@ session_start();
     </div>
   </main>
   
-  <footer class="pd-flex min-vh-50 mt-auto justify-content-end align-items-end py-4 border-dark border-5 bg-dark bg-gradient">
+  <footer class="pd-flex min-vh-50 mt-auto justify-content-end align-items-end py-4 border-5 foot">
     <div class="d-flex justify-content-center">
     <ul class="nav col-md-4 justify-content-center">
-      <li class="nav-item"><a href="#" class="nav-link text-end text-white-50">Elena Moreno</a></li>
+      <li class="nav-item"><a href="#" class="nav-link text-black">Elena Moreno</a></li>
     </ul>
     </div>
   </footer>
